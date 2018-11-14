4688988c10ac32ccf5112696e2983e148271e7f1
Danny Peck <dannytpeck@gmail.com>
2018-11-14 12:58:53 -0800

    Allow commas in event names

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -95,7 +95,7 @@ class App extends Component {
       const cie = [
         employerName,
         eventId,
-        eventName,
+        '"' + eventName + '"',
         '',
         'IncentivePoints',
         pointsAwarded.replace(',', ''),
