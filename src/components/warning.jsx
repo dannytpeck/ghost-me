ccce10a60968f4f41cfd87dfcada9fe0b2eb4d28
Danny Peck <dannytpeck@gmail.com>
2022-06-08 22:05:13 -0700

    Update warning to functional component

--- src/components/warning.jsx
+++ src/components/warning.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Warning extends Component {
-  render() {
-    return (
-      <div id="warning" className="text-danger">
-        <p><em>WARNING: Double-check the Event ID, because this app will overwrite whatever is currently stored if you use an ID that's already in-use.<br/>
-        It's a great way to update already existing IDs but could be disastrous if you erase a custom CIE you didn't intend to overwrite!</em></p>
-      </div>
-    );
-  }
+const Warning = () => {
+  return (
+    <div id="warning" className="text-danger">
+      <p>
+        <em>WARNING: Double-check the Event ID, because this app will overwrite whatever is currently stored if you use an ID that's already in-use.</em>
+      </p>
+    </div>
+  );
 }
 
 export default Warning;
